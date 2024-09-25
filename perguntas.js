4e97bfd94ef81541f24622e7339236152aea2733
Anajhullytx <160230806+Anajhullytx@users.noreply.github.com>
2024-09-25 07:55:18 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,32 @@
+criaCartao(
+    'Geografia',
+    'Capital do Brasil?',
+    'Qual cidade bela do Brasil?',
+    'A cidade bela do Brasil se chama Curitiba'
+)
+
+criaCartao(
+    'Geografia',
+    'Capital do Brasil?',
+    'Qual cidade bela do Brasil?',
+    'A cidade bela do Brasil se chama Curitiba'
+)
+
+criaCartao(
+    'Geografia',
+    'Capital do Brasil?',
+    'Qual cidade bela do Brasil?',
+    'A cidade bela do Brasil se chama Curitiba',
+)
+
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
