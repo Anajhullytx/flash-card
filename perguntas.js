fe61d52fd913dfe22328519eaeb467577cb0d067
Anajhullytx <160230806+Anajhullytx@users.noreply.github.com>
2024-10-09 07:57:37 -0300

    Add files via upload

--- perguntas.js
+++ perguntas.js
@@ -1,32 +1,31 @@
 criaCartao(
     'Geografia',
     'Qual a cidade mais bela do Brasil?',
-    'Qual cidade bela do Brasil?',
-    'A cidade bela do Brasil se chama Curitiba'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a cidade mais bela do Brasil?',
-    'Qual cidade bela do Brasil?',
-    'A cidade bela do Brasil se chama Curitiba'
-)
-
-criaCartao(
-    'Geografia',
-    'Qual a cidade mais bela do Brasil?',
-    'Qual cidade bela do Brasil?',
     'A cidade bela do Brasil se chama Curitiba',
 )
 
+criaCartao(
+    'Geografia',
+    'De quem é a famosa frase "Penso, logo existo"?',
+    'A famosa frase se chama Descartes',
+    
+)
 
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'De onde é a invenção do chuveiro elétrico?',
+    'Foi no Brasil',
+    
+)
+
+
+criaCartao(
+    'Geografia',
+    'Qual o menor e o maior país do mundo?',
+    'Vaticano e Rússia',
 )
 criaCartao(
-    'Lingua inglesa',
-    'Como se diz oi em Inglês?',
-    'Oi em ingles é HI (RAI)'
+    'Geografia',
+    'Quantas casas decimais tem o número pi?',
+    'Infinitas',
 )
